ecb60f61f2e4987bf75cc2c8d8a1b55ed6e2e54c
Matt Glover <matt@duchysoftware.com>
2016-02-01 15:42:21 +0100

    Initial Commit

---
+++ ActivateConstraints/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ActivateConstraints
+//
+//  Created by Matt Glover on 01/02/2016.
+//  Copyright © 2016 Duchy Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ActivateConstraints/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ActivateConstraints
+//
+//  Created by Matt Glover on 01/02/2016.
+//  Copyright © 2016 Duchy Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
